637e3d279349f286572f1a79913cc63cc134d3ca
Alessio Marchi <65831826+kettei-sproutty@users.noreply.github.com>
2025-03-14 14:23:19 +0100

    feat: add constructor options

--- src/lib.rs
+++ src/lib.rs
@@ -7,21 +7,31 @@ use wasm_bindgen::prelude::*;
 use web_sys::{BlobPropertyBag, HtmlElement};
 
 #[wasm_bindgen(typescript_custom_section)]
-const IGENERATE_OPTIONS: &'static str = r#"
-interface IGenerateOptions<T = any> {
+const IAPPEND_OPTIONS: &'static str = r#"
+export interface IAppendOptions<T = any> {
   sheetName: string;
-   data: T[];
+  data: T[];
   headers: Header[];
 }
 
-interface Header {
+export interface Header {
   key: string;
   label: string;
 }
 "#;
 
+#[wasm_bindgen(typescript_custom_section)]
+const IGENERATE_OPTIONS: &'static str = r#"
+export interface IGenerateOptions {
+  failOnDuplicate: boolean;
+}
+"#;
+
 #[wasm_bindgen]
 extern "C" {
+  #[wasm_bindgen(typescript_type = "IAppendOptions")]
+  pub type IAppendOptions;
+
   #[wasm_bindgen(typescript_type = "IGenerateOptions")]
   pub type IGenerateOptions;
 }
@@ -33,36 +43,77 @@ pub struct Header {
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct ConverterOptions {
+pub struct AppendOptions {
   #[serde(rename(deserialize = "sheetName"))]
   sheet_name: Option<alloc::string::String>,
   data: alloc::vec::Vec<serde_json::Value>,
   headers: alloc::vec::Vec<Header>,
 }
 
+fn default_fail_on_duplicate() -> bool {
+  true
+}
+
+#[derive(Serialize, Deserialize)]
+struct GenerateOptions {
+  #[serde(rename(deserialize = "failOnDuplicate"))]
+  #[serde(default = "default_fail_on_duplicate")]
+  /// If false, it will append a random string to the sheet name to make it unique if a sheet with the same name already exists.
+  /// @default true
+  fail_on_duplicate: bool,
+}
+
+impl Default for GenerateOptions {
+  fn default() -> Self {
+    Self {
+      fail_on_duplicate: default_fail_on_duplicate(),
+    }
+  }
+}
+
 #[wasm_bindgen]
 pub struct Converter {
+  options: GenerateOptions,
   workbook: umya_spreadsheet::Spreadsheet,
 }
 
 #[wasm_bindgen]
 impl Converter {
   #[wasm_bindgen(constructor)]
-  pub fn new() -> Self {
+  pub fn new(options: Option<IGenerateOptions>) -> Self {
     set_panic_hook();
+
+    let options: GenerateOptions = options
+      .map(|options| serde_wasm_bindgen::from_value(options.into()).unwrap())
+      .unwrap_or_default();
+
     let workbook = umya_spreadsheet::new_file_empty_worksheet();
 
     Self {
+      options,
       workbook,
     }
   }
 
-  pub fn append(&mut self, options: IGenerateOptions) {
-    let options: ConverterOptions =
+  pub fn append(&mut self, options: IAppendOptions) {
+    let options: AppendOptions =
       serde_wasm_bindgen::from_value(options.into()).unwrap();
     let sheet_index = self.workbook.get_sheet_count();
-    let sheet_name =
-      options.sheet_name.unwrap_or(alloc::format!("Sheet{}", sheet_index + 1));
+    let mut sheet_name = options.sheet_name.unwrap_or(alloc::format!("Sheet{}", sheet_index + 1));
+
+    if self.options.fail_on_duplicate {
+      if self.workbook.get_sheet_by_name(&sheet_name).is_some() {
+        panic!("Sheet with the same name already exists");
+      }
+    } else {
+      let mut index = 1;
+      while self.workbook.get_sheet_by_name(&sheet_name).is_some() {
+        sheet_name = alloc::format!("{} ({})", sheet_name, index);
+        index += 1;
+      }
+    }
+
+    let sheet_name = sheet_name;
 
     let worksheet =
       self.workbook.new_sheet(sheet_name).expect("Failed to create new sheet");
@@ -95,7 +146,7 @@ impl Converter {
     }
   }
 
-  pub async fn append_async(&mut self, options: IGenerateOptions) {
+  pub async fn append_async(&mut self, options: IAppendOptions) {
     self.append(options);
   }
 
